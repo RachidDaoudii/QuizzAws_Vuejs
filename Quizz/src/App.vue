b0b6f5a851fc6bdea3d655ecb9b1c6d1128d56e1
rachiddaoudi <rachiddaoudi533@gmail.com>
2023-03-10 18:02:32 +0100

    first version

---
+++ Quizz/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import Stepper from './components/stepper.vue'
+import StartQuizz from './components/StartQuizz.vue'
+import ResultQuizz from './components/ResultQuizz.vue'
+</script>
+
+<template>
+  <Stepper/>
+  <StartQuizz/>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ Quizz/src/components/StartQuizz.vue
@@ -0,0 +1,368 @@
+<template>
+  <div class="container mt-5">
+      <div class="d-flex justify-content-center row">
+            <div class="logo d-flex justify-content-center">
+              <img class="w-25" src="../assets/img/logo.png"  alt="">
+            </div>
+          <div class="col-md-10 col-lg-10">
+              <div class="border">
+                  <div class="question bg-white p-3 border-bottom">
+                      <div class="d-flex flex-row justify-content-between align-items-center mcq">
+                        <h4>Quiz</h4><span>( {{ currentQuestion+1 }} of 10)</span>
+                      </div>
+                  </div>
+                  <div class="question bg-white p-3 border-bottom"
+                  
+                  >
+                      <div class="d-flex flex-row align-items-center question-title">
+                          <h3 class="text-danger px-4">Q.{{ currentQuestion+1 }}</h3>
+                          <h5 class="mt-1 ml-2">{{ getQuestion.content  }}</h5>
+                      </div>
+                      <div class="ans ml-2"
+                      v-for="(option,index) in getQuestion.options"
+                      :key="index"
+                      >
+                        <label class="radio"><input type="radio" 
+                          :name="currentQuestion.index" 
+                          :value="option.id"
+                          @click="response(option.id)"
+                          > <span>{{ option.content }}</span>
+                        </label>    
+                      </div>
+                  </div>
+                  <div class="d-flex flex-row justify-content-center align-items-center p-3 bg-white">
+                    <button class="btn btn-primary border-success align-items-center btn-success"
+                    @click="nextQuestion"
+                    >next
+                    </button>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<style>
+.logo{
+  width: 360px;
+}
+</style>
+<script setup>
+import {ref,computed} from 'vue'
+const data = ref([
+  {
+      "title":"AWS - Quiz",
+      "description":"AWS Certified Cloud Practitioner (CLF-C01) Sample Exam Questions",
+      "questions":[
+          {
+            "id":1,
+            "content":"Why is AWS more economical than traditional data centers for applications with varying compute workloads?",
+            "options":[
+              {
+                "id":1,
+                "content":"Amazon EC2 costs are billed on a monthly basis"
+              },
+              {
+                "id":2,
+                "content":"Users retain full administrative access to their Amazon EC2 instances. "
+              },
+              {
+                "id":3,
+                "content":"Amazon EC2 instances can be launched on demand when needed."
+              },
+              {
+                "id":4,
+                "content":"Users can permanently run enough instances to handle peak workloads."
+              }
+            ],
+            "answer":{
+              "correct":3,
+              "comment":"The ability to launch instances on demand when needed allows users to launch and terminate instances in response to a varying workload. This is a more economical practice than purchasing enough on-premises serversto handle the peak load"
+            }
+          },
+          {
+            "id":2,
+            "content":"Which AWS service would simplify the migration of a database to AWS?",
+            "options":[
+              {
+                "id":1,
+                "content":"AWS Storage Gateway "
+              },
+              {
+                "id":2,
+                "content":"AWS Database Migration Service (AWS DMS)."
+              },
+              {
+                "id":3,
+                "content":"Amazon EC2"
+              },
+              {
+                "id":4,
+                "content":"Amazon AppStream 2.0"
+              }
+            ],
+            "answer":{
+              "correct":2,
+              "comment":"AWS DMS helps users migrate databases to AWS quickly and securely. The source database remains fully operational during the migration, minimizing downtime to applications that rely on the database. AWS DMS can migrate data to and from most widely used commercial and open-source databases."
+            }
+          },
+          {
+              "id":3,
+              "content":"Which AWS offering enables users to find, buy, and immediately start using software solutions in their AWS environment?",
+              "options":[
+                  {
+                      "id":1,
+                      "content":"AWS Config"
+                  },
+                  {
+                      "id":2,
+                      "content":"AWS OpsWorks "
+                  },
+                  {
+                      "id":3,
+                      "content":"AWS SDK"
+                  },
+                  {
+                      "id":4,
+                      "content":"AWS Marketplace"
+                  }
+              ],
+              "answer":{
+                  "correct":4,
+                  "comment":" AWS Marketplace is a digital catalog with thousands of software listings from independent software vendors that makes it easy to find, test, buy, and deploy software that runs on AWS."
+              }
+          },
+          {
+              "id":4,
+              "content":"Which AWS networking service enables a company to create a virtual network within AWS?",
+              "options":[
+                  {
+                      "id":1,
+                      "content":"AWS Config"
+                  },
+                  {
+                      "id":2,
+                      "content":") Amazon Route 53"
+                  },
+                  {
+                      "id":3,
+                      "content":"AWS Direct Connect"
+                  },
+                  {
+                      "id":4,
+                      "content":"Amazon Virtual Private Cloud (Amazon VPC)"
+                  }
+              ],
+              "answer":{
+                  "correct":4,
+                  "comment":"Amazon VPC lets users provision a logically isolated section of the AWS Cloud where users can launch AWS resources in a virtual network that they define."
+              }
+          },
+          {
+              "id":5,
+              "content":" Which of the following is an AWS responsibility under the AWS shared responsibility model?",
+              "options":[
+                  {
+                      "id":1,
+                      "content":"Configuring third-party applications"
+                  },
+                  {
+                      "id":2,
+                      "content":"Maintaining physical hardware"
+                  },
+                  {
+                      "id":3,
+                      "content":"Securing application access and data."
+                  },
+                  {
+                      "id":4,
+                      "content":"Managing guest operating systems "
+                  }
+              ],
+              "answer":{
+                  "correct":2,
+                  "comment":"Maintaining physical hardware is an AWS responsibility under the AWS shared responsibility model."
+              }
+          },
+          {
+              "id":6,
+              "content":"Which component of the AWS global infrastructure does Amazon CloudFront use to ensure low-latencydelivery? ",
+              "options":[
+                  {
+                      "id":1,
+                      "content":"AWS Regions"
+                  },
+                  {
+                      "id":2,
+                      "content":"Edge locations"
+                  },
+                  {
+                      "id":3,
+                      "content":"Availability Zones"
+                  },
+                  {
+                      "id":4,
+                      "content":"Virtual Private Cloud (VPC)"
+                  }
+              ],
+              "answer":{
+                  "correct":2,
+                  "comment":"To deliver content to users with lower latency, Amazon CloudFront uses a global network of points of presence (edge locations and regional edge caches) worldwide. "
+              }
+          },
+          {
+              "id":7,
+              "content":"How would a system administrator add an additional layer of login security to a user's AWS Management Console?",
+              "options":[
+                  {
+                      "id":1,
+                      "content":"Use Amazon Cloud Directory"
+                  },
+                  {
+                      "id":2,
+                      "content":"Audit AWS Identity and Access Management (IAM) roles "
+                  },
+                  {
+                      "id":3,
+                      "content":"Enable multi-factor authentication"
+                  },
+                  {
+                      "id":4,
+                      "content":"Enable AWS CloudTrail"
+                  }
+              ],
+              "answer":{
+                  "correct":3,
+                  "comment":"Multi-factor authentication (MFA) is a simple best practice that adds an extra layer of protection on top of a  username and password. With MFA enabled, when a user signs in to an AWS Management Console, they will be prompted for their username and password (the first factor—what they know), as well as for an authentication code from their MFA device (the second factor—what they have). Taken together, these multiple factors provide increased security for AWS account settings and resources."
+              }
+          },
+          {
+              "id":8,
+              "content":"Which service can identify the user that made the API call when an Amazon EC2 instance is terminated?",
+              "options":[
+                  {
+                      "id":1,
+                      "content":"AWS Trusted Advisor"
+                  },
+                  {
+                      "id":2,
+                      "content":"AWS CloudTrail "
+                  },
+                  {
+                      "id":3,
+                      "content":"AWS X-Ray"
+                  },
+                  {
+                      "id":4,
+                      "content":"AWS Identity and Access Management (AWS IAM)"
+                  }
+              ],
+              "answer":{
+                  "correct":2,
+                  "comment":"AWS CloudTrail helps users enable governance, compliance, and operational and risk auditing of their AWS accounts. Actions taken by a user, role, or an AWS service are recorded as events in CloudTrail. Events include actions taken in the AWS Management Console, AWS Command Line Interface (CLI), and AWS SDKs and APIs."
+              }
+          },
+          {
+              "id":9,
+              "content":"Which service would be used to send alerts based on Amazon CloudWatch alarms?",
+              "options":[
+                  {
+                      "id":1,
+                      "content":"Amazon Simple Notification Service (Amazon SNS)"
+                  },
+                  {
+                      "id":2,
+                      "content":"AWS CloudTrail "
+                  },
+                  {
+                      "id":3,
+                      "content":"AWS Trusted Advisor"
+                  },
+                  {
+                      "id":4,
+                      "content":"Amazon Route 53"
+                  }
+              ],
+              "answer":{
+                  "correct":1,
+                  "comment":"Multi-factor authentication (MFA) is a simple best practice that adds an extra layer of protection on top of a  username and password. With MFA enabled, when a user signs in to an AWS Management Console, they will be prompted for their username and password (the first factor—what they know), as well as for an authentication code from their MFA device (the second factor—what they have). Taken together, these multiple factors provide increased security for AWS account settings and resources."
+              }
+          },
+          {
+              "id":10,
+              "content":"Where can a user find information about prohibited actions on the AWS infrastructure?",
+              "options":[
+                  {
+                      "id":1,
+                      "content":"AWS Trusted Advisor"
+                  },
+                  {
+                      "id":2,
+                      "content":"AWS Identity and Access Management (IAM) "
+                  },
+                  {
+                      "id":3,
+                      "content":"AWS Billing Console"
+                  },
+                  {
+                      "id":4,
+                      "content":"AWS Acceptable Use Policy"
+                  }
+              ],
+              "answer":{
+                  "correct":4,
+                  "comment":"The AWS Acceptable Use Policy provides information regarding prohibited actions on the AWS infrastructure. "
+              }
+          }
+          
+      ]
+  }
+]);
+
+
+const score = computed(()=> {
+  // let val = 0
+  // question.value.map(Q =>{
+  //   if(Q.options.id == Q.answer.correct){
+  //     val++
+  //   }
+  //   return val
+  // })
+  let val = 0
+  return val
+})
+
+const currentQuestion = ref(0);
+
+const nextQuestion = ()=>{
+  ReponesVrai()
+  if(currentQuestion.value < data.value[0].questions.length -1){
+    currentQuestion.value++
+  }else{
+    console.log('gfdgfd');
+  }
+}
+
+const getQuestion = computed(()=>{
+  let question = data.value[0].questions[currentQuestion.value]
+  return question
+})
+
+
+let checkReponse =[];
+
+var checkedOption = null
+
+const response = (id)=>{
+  checkedOption = id
+}
+
+const ReponesVrai = () =>{
+  if(getQuestion.value.answer.correct != checkedOption){
+    checkReponse.push(getQuestion.value.id)
+  }
+}
+
+
+
+</script>
